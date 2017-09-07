c815262b12646a08d77e41956f2f4a36d80cd021
Christopher Tatli <christopher.tatli@gmail.com>
2017-09-07 15:55:03 +1000

    Updated Friend Request screen to have the same UI as Friend List

--- client/src/screens/FriendRequest.js
+++ client/src/screens/FriendRequest.js
@@ -5,8 +5,9 @@ import {
    StyleSheet,
    Text,
    View,
-   Button
+   Image
 } from 'react-native';
+import {Button} from 'react-native-elements';
 import NavIcons from "../components/NavIcons";
 import {NavigationActions} from 'react-navigation';
 
@@ -17,35 +18,49 @@ export default class FriendRequest extends Component{
       headerLeft: NavIcons.closeButton(navigation.goBack)
    });
 
-   constructor(props){
+   constructor(props) {
       super(props);
       this.store = this.props.screenProps.store;
       this.requests = [
-         {
-            name: "Chris",
-         },
-         {
-            name:"Marco",
-         }
+        {
+          _id: "59af9b954a6fb439b3950521",
+          name: "Marco",
+          avatar: "https://www.gravatar.com/avatar/0a9c48e5aae9e009e099bf46bca361d0?s=60&d=retro"
+        },
+        {
+          _id: "59afaeb64a6fb439b395053a",
+          name: "William",
+          avatar: "https://www.gravatar.com/avatar/6340835627f09b4e97c16e78e4dc3b08?s=60&d=retro"
+        }
       ];
-   }
+    }
 
-   generateFriendRequestList(requests){
-      return requests.map((request) =>
-      <View style={{flex: 2, flexDirection: 'row', margin:5, paddingLeft:10, paddingRight:10}}>
-        <Text style={{fontSize:20, textAlign:'left'}} >{request.name}</Text>
-        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'flex-end'}}>
-          <Button
-            title="Accept"
-            style={{padding:5}}
-          />
-          <Button
-            title="Decline"
-
-          />
-        </View>
-      </View>
-      );
+    generateFriendRequestList(requests) {
+     return requests.map((request) =>
+     <View key={request._id} style={{flex: 2, flexDirection: 'row', margin:5, paddingLeft:10,
+       paddingRight:10, paddingBottom: 10, borderBottomWidth: 1, borderBottomColor: '#48fdf6'}}>
+       <Image source={{uri: request.avatar}} style={styles.avatar} />
+       <Text style={{fontSize:18, textAlign:'left', color: '#48fdf6'}} >{request.name}</Text>
+       <View style={{flex: 1, flexDirection: 'row', justifyContent: 'flex-end'}}>
+         <Button title='accept'
+           onPress={() => {}}
+           backgroundColor='#48fdf6'
+           color={'black'}
+           fontSize={10}
+           buttonStyle={styles.button}
+           >
+         </Button>
+         <Button title='decline'
+           onPress={() => {}}
+           backgroundColor='#48fdf6'
+           color={'black'}
+           fontSize={10}
+           buttonStyle={styles.button}
+           >
+         </Button>
+       </View>
+     </View>
+     );
    }
 
    render(){
@@ -58,9 +73,25 @@ export default class FriendRequest extends Component{
 }
 
 const styles = StyleSheet.create({
-   container: {
-      flex:1,
-      flexDirection: 'column',
-      paddingTop:5
-   },
-});
+  container: {
+   flex: 1,
+   flexDirection: 'column',
+   paddingTop: 5,
+   backgroundColor: 'black'
+  },
+  button: {
+    borderRadius: 5,
+    borderWidth: 0,
+    borderColor: 'black',
+    width: 80,
+    height: 30,
+    marginRight: 0
+  },
+  avatar: {
+    resizeMode: 'contain',
+    width: 25,
+    height: 25,
+    borderRadius: 50,
+    marginRight: 10
+  }
+})
