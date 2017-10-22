1616760e1f68f2d73fb39a5efaff4c26838737f4
Marco <marcoavermaak@gmail.com>
2017-10-23 08:53:37 +1100

    Final Documentation

--- server/src/services/users/users.hooks.js
+++ server/src/services/users/users.hooks.js
@@ -27,8 +27,10 @@ module.exports = {
 		hashPassword(), 
 		hook => {
 			const { email } = hook.data;
-			const hash = crypto.createHash('md5').update(email).digest('hex');
-
+			// obtain md5 hash of user email
+      const hash = crypto.createHash('md5').update(email).digest('hex');
+      // set user avatar field to the gravatar URL which is used to fetch the
+      // gravatar image for that user
 			hook.data.avatar = `${gravatarUrl}${hash}?${query}`;
 		}	
 		],
@@ -41,6 +43,7 @@ module.exports = {
     all: [
       commonHooks.when(
         hook => hook.params.provider,
+        // Don't include password hash in response data
         commonHooks.discard('password')
       )
     ],
@@ -49,8 +52,10 @@ module.exports = {
     create: [],
     update: [
       hook => {
+        // Friend is removed from user document
         if(JSON.stringify(hook.data).includes('{"$pull":{"friends"')) {
           
+          // First search if this user is still in removed user's friend list
           hook.app.service('users').get(hook.data.$pull.friends._id).then(result => {
             let found = false;
             let friends = result.friends;
@@ -60,6 +65,9 @@ module.exports = {
                 break;
               }
             }
+
+            // If true then also remove this user from other user's friend
+            // list - this is to avoid cyclic calls to update
             if(found) {
               hook.app.service('users').update(result._id,
                  { $pull: { friends: { username: hook.params.user.username } } })
@@ -84,6 +92,8 @@ module.exports = {
     find: [],
     get: [],
     create: [
+      // Return error messages if unique constraints for email / username are
+      // violated
       hook => {
        if(`${hook.error}`.includes('email')) {
         throw new Error('Email already in use.');
